b44647cfe2962598686344b7eba2ebf68c276258
AmulyaPamidimukkala <Amulya.Pamidimukkala@gmail.com>
2020-04-18 03:06:50 -0500

    refactor

--- client/src/components/game.component.jsx
+++ client/src/components/game.component.jsx
@@ -1,18 +1,34 @@
-import React  from 'react'
-import { EntryComponent } from './entry.component'
+import React from 'react'
+import { AddPlayer } from './addPlayer'
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button } from 'reactstrap';
+
 class GameComponent extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = { players: [] };
+        this.addPlayer = this.addPlayer.bind(this);
     }
 
-
+    addPlayer(playerName) {
+        if (playerName.length === 0) {
+            return;
+        }
+        const newItem = {
+            text: playerName,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            players: state.players.concat(newItem),
+            text: ''
+        }));
+    }
     render() {
         return (
-            <div className="game">
+            <div className="game" >
                 <h1>Judgement</h1>
-            <EntryComponent players={this.state.players}/>
+                <AddPlayer players={this.state.players} addPlayer={this.addPlayer} />
+                {this.state.players.length >= 5 ? <Button className="btn btn-block btn-lg btn-success mb-4 col-md-5">Start game</Button> : null}
             </div>
         );
     }
