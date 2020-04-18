15c73ff4401ed8769b28bfe235f78e4d400d5ad6
AmulyaPVS <amulya@elpaviation.com>
2020-04-18 01:19:05 -0500

    Added Bootstrap

---
+++ client/src/components/game.component.jsx
@@ -0,0 +1,15 @@
+import React, {
+    Component
+} from 'react'
+class GameComponent extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <h1>Judgement</h1>
+
+            </div>
+        );
+    }
+}
+
+export default GameComponent;
