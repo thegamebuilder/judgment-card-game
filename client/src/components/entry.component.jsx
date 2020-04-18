c2a558fb89cd228f3013a162da695264434bd774
AmulyaPVS <amulya@elpaviation.com>
2020-04-18 02:36:27 -0500

    Player component

---
+++ client/src/components/entry.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { InputGroup, InputGroupAddon, InputGroupText, Input, Button } from 'reactstrap';
+
+export class EntryComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { players: [], text: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.state.text.length === 0) {
+            return;
+        }
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            players: state.players.concat(newItem),
+            text: ''
+        }));
+    }
+
+    render() {
+        return (
+            <div className="col-md-5" >
+                <form onSubmit={this.handleSubmit}>
+                    <Input placeholder="Enter player Name" className="mb-4" onChange={this.handleChange}
+                        value={this.state.text} />
+                    {this.state.players.length < 5 ? <Button className="btn btn-block btn-lg btn-warning mb-4">Add player</Button> : null}
+                </form>
+                <PlayerList items={this.state.players} />
+                {this.state.players.length >= 5 ? <Button className="btn btn-block btn-lg btn-success mb-4">Start game</Button> : null}
+            </div>
+        )
+    }
+}
+
+class PlayerList extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+        );
+    }
+}
--- client/src/components/game.component.jsx
+++ client/src/components/game.component.jsx
@@ -1,12 +1,18 @@
-import React, {
-    Component
-} from 'react'
+import React  from 'react'
+import { EntryComponent } from './entry.component'
 class GameComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { players: [] };
+    }
+
+
     render() {
         return (
             <div className="game">
                 <h1>Judgement</h1>
-
+            <EntryComponent players={this.state.players}/>
             </div>
         );
     }
